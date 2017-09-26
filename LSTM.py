3b761e34f24f9b1f8bea9a95429db5791eb39a69
Pratik Sen <prateek.sen.14cse@bml.edu.in>
2017-09-26 15:39:34 +0530

    Add files via upload

---
+++ LSTM.py
@@ -0,0 +1,96 @@
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+import time
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset
+dataframe = pandas.read_csv('GOOGL_stock_2017-05-07.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+
+# reshape into X=t and Y=t+1
+look_back = 30
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+start_time = time.time()
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(30, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(trainX, trainY, epochs=10, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# shift train predictions for plotting
+
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+'''
+diff=[]
+ratio=[]
+p = model.predict(testX)
+for u in range(len(testY)):
+    pr = p[u][0]
+    ratio.append((testY[u]/pr)-1)
+    diff.append(abs(testY[u]- pr))
+    #print(u, y_test[u], pr, (y_test[u]/pr)-1, abs(y_test[u]- pr))
+'''
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(testPredictPlot,color='red')
+
